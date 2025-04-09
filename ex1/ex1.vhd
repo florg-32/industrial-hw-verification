d6ad296d6eae899f9a0ff40d5dd6bdeb2dc799bc
Florian Guggi <f.guggi@viewpointsystem.com>
2025-04-09 10:30:11 +0200

    feat: Implement exercise 1 & 2

--- ex1/ex1.vhd
+++ ex1/ex1.vhd
@@ -10,11 +10,21 @@ architecture behav of ex1 is
 
   pure function ColorToString(pos: integer) return string is
   begin
-    -- Enter your code here
+    if (pos < 0) or (pos > ColorT'pos(ColorT'right)) then
+      Alert("color index out of range");
+      return "OutOfRange";
+    end if;
+    return to_string(ColorT'val(pos));
   end function;
 
   pure function ColorsToList return string is
-    -- Enter your code here
+    type string_ptr is access string;
+    variable ret : string_ptr := new string'(ColorToString(0));
+  begin
+    for i in 1 to ColorT'pos(ColorT'right) loop
+      ret := new string'(ret.all & ", " & ColorToString(i));
+    end loop;
+    return ret.all;
   end function;
 
 begin
@@ -23,7 +33,8 @@ begin
   begin
     -- During initialization, the process is evaluated until the first wait statement
     wait for 0 ns;
-    -- Enter your code here
+    report ColorToString(1) severity note;
+    report "AllEnums: " & ColorsToList severity note;
     Log("**********************************");
     ReportAlerts;
     std.env.stop;
